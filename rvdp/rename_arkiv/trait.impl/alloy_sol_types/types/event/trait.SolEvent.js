7228fa7b46f7dc15b2c5faec9780176323ab8bce
r-vdp <r-vdp@users.noreply.github.com>
2025-09-30 15:55:37 +0000

    deploy: c8690f356db36651f9808dee728f3f89ee98dde7

--- rvdp/rename_arkiv/trait.impl/alloy_sol_types/types/event/trait.SolEvent.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var implementors = Object.fromEntries([["arkiv_sdk",[["impl SolEvent for <a class=\"struct\" href=\"arkiv_sdk/eth/ArkivABI/struct.GolemBaseStorageEntityBTLExtended.html\" title=\"struct arkiv_sdk::eth::ArkivABI::GolemBaseStorageEntityBTLExtended\">GolemBaseStorageEntityBTLExtended</a>"],["impl SolEvent for <a class=\"struct\" href=\"arkiv_sdk/eth/ArkivABI/struct.GolemBaseStorageEntityCreated.html\" title=\"struct arkiv_sdk::eth::ArkivABI::GolemBaseStorageEntityCreated\">GolemBaseStorageEntityCreated</a>"],["impl SolEvent for <a class=\"struct\" href=\"arkiv_sdk/eth/ArkivABI/struct.GolemBaseStorageEntityDeleted.html\" title=\"struct arkiv_sdk::eth::ArkivABI::GolemBaseStorageEntityDeleted\">GolemBaseStorageEntityDeleted</a>"],["impl SolEvent for <a class=\"struct\" href=\"arkiv_sdk/eth/ArkivABI/struct.GolemBaseStorageEntityUpdated.html\" title=\"struct arkiv_sdk::eth::ArkivABI::GolemBaseStorageEntityUpdated\">GolemBaseStorageEntityUpdated</a>"]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":57,"fragment_lengths":[919]}
