4374bc1b72eb27565290a6b7dc36ca246e653ea6
jeffcogswell <jeffcogswell@users.noreply.github.com>
2025-10-23 19:12:07 +0000

    deploy: 8f5fd0739ddd415fe663455e6aeddba35bb42e8b

---
+++ litepaper-patch-1/trait.impl/core/convert/trait.TryFrom.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["arkiv_sdk",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.88.0/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;Log&gt; for <a class=\"enum\" href=\"arkiv_sdk/events/enum.Event.html\" title=\"enum arkiv_sdk::events::Event\">Event</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.88.0/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;TransactionReceipt&gt; for <a class=\"struct\" href=\"arkiv_sdk/entity/struct.TransactionResult.html\" title=\"struct arkiv_sdk::entity::TransactionResult\">TransactionResult</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[628]}
